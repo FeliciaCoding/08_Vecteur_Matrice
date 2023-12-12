89365e9a391113c7a4e4225d48f734e949e1af19
Reynard Paul <reynardpaul92@gmail.com>
2023-12-12 16:02:21 +0100

    Create branch reynardpaul

--- main.cpp
+++ main.cpp
@@ -1,25 +1,47 @@
 #include <iostream>
 #include <cstdlib>
 #include <vector>
-
+#include <span>
+#include <vector>
 
 using namespace std;
+
+size_t lineSizeMin(){
+    size_t result = 0;
+    return result;
+};
+
+size_t sortMatrix(){
+    size_t result = 0;
+    return result;
+};
+
+void lineSizeMax (){
+
+}
+
+template<typename T>
+ostream &operator<<(ostream &os, span<T> s) {
+    os << "[";
+    for (size_t i = 0; i < s.size(); ++i) {
+        if (i) os << ", ";
+        os << s[i];
+    }
+    return os << "]";
+}
 
 int main() {
 
     // matrice        : [[5, 2, 8], [4, 3, 9], [1]]
+    vector< vector<int> > m;
+    cout << m << endl;
+
 
 
     // isSquare       : no
 
 
     // isRegular      : no
-
-
-    // lineSizeMin    : 1
-
-
-    // lineSizeMax    : 3
 
 
     // vectSumLine    : [15, 16, 1]
@@ -33,9 +55,10 @@ int main() {
 
     // shuffleMatrix  : [[1], [4, 3, 9], [5, 2, 8]]	// exemple
 
-
+    // PRE
+    // lineSizeMin    : 1
     // sortMatrix     : [[1], [5, 2, 8], [4, 3, 9]]
-
+    // lineSizeMax    : 3
 
 
 
