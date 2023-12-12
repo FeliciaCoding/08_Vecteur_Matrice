f390bf8bcf1e1fb21e74f9468b4557ce66959307
FeliciaCoding <49447678+FeliciaCoding@users.noreply.github.com>
2023-12-12 15:32:08 +0100

    Outlines

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+
+
+using namespace std;
+
+int main() {
+
+    // matrice        : [[5, 2, 8], [4, 3, 9], [1]]
+
+
+    // isSquare       : no
+
+
+    // isRegular      : no
+
+
+    // lineSizeMin    : 1
+
+
+    // lineSizeMax    : 3
+
+
+    // vectSumLine    : [15, 16, 1]
+
+
+    // vectSumColumn  : [10, 5, 17]
+
+
+    // vectorSumMin   : [1]
+
+
+    // shuffleMatrix  : [[1], [4, 3, 9], [5, 2, 8]]	// exemple
+
+
+    // sortMatrix     : [[1], [5, 2, 8], [4, 3, 9]]
+
+
+
+
+    return EXIT_SUCCESS;
+}
