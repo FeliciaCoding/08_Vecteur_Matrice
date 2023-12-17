adce1deae695ce01f4780097127c0ab655d31eb3
FeliciaCoding <49447678+FeliciaCoding@users.noreply.github.com>
2023-12-17 20:08:41 +0100

    moving <usingnamespace std;> after functions to simplify future library creation

--- main.cpp
+++ main.cpp
@@ -6,22 +6,20 @@
 #include <vector>
 #include <random>
 
-using namespace std;
-
-const string MATRICE_TITLE      = "matrice";
-const string SQUARE_TITLE       = "isSquare";
-const string REGULAR_TITLE      = "isRegular";
-const string LINE_MIN_TITLE     = "lineSizeMin";
-const string LINE_MAN_TITLE     = "lineSizeMax";
-const string VECT_LINE_TITLE    = "vectSumLine";
-const string VECT_COL_TITLE     = "vectSumColumn";
-const string VECT_MIN_TITLE     = "vectorSumMin";
-const string SHUFFLE_MRIX_TITLE = "shuffleMatrix";
-const string SORT_MRIX_TITLE    = "sortMatrix";
+const std::string MATRICE_TITLE      = "matrice";
+const std::string SQUARE_TITLE       = "isSquare";
+const std::string REGULAR_TITLE      = "isRegular";
+const std::string LINE_MIN_TITLE     = "lineSizeMin";
+const std::string LINE_MAN_TITLE     = "lineSizeMax";
+const std::string VECT_LINE_TITLE    = "vectSumLine";
+const std::string VECT_COL_TITLE     = "vectSumColumn";
+const std::string VECT_MIN_TITLE     = "vectorSumMin";
+const std::string SHUFFLE_MRIX_TITLE = "shuffleMatrix";
+const std::string SORT_MRIX_TITLE    = "sortMatrix";
 
 
 template<typename T>
-ostream &operator<<(ostream &os, vector<T> v) { //operator<<	Affiche un vecteur au format
+std::ostream &operator<<(std::ostream &os, std::vector<T> v) { //operator<<	Affiche un vecteur au format
     os << "[";
     for_each(v.begin(), v.end() - 1, [&os](int i) { os << i << ','; });
     os << v[v.size() - 1];
@@ -29,62 +27,56 @@ ostream &operator<<(ostream &os, vector<T> v) { //operator<<	Affiche un vecteur 
 }
 
 template<typename T>
-ostream &operator<<(ostream &os, vector<vector<T>> v) { //operator<<	Affiche un vecteur au format
+std::ostream &operator<<(std::ostream &os, std::vector<std::vector<T>> v) { //operator<<	Affiche un vecteur au format
     os << "[";
-
-    for_each(v.begin(), v.end() - 1, [&os](vector<T> i) { os << i << ','; });
+    for_each(v.begin(), v.end() - 1, [&os](std::vector<T> i) { os << i << ','; });
     os << v[v.size() - 1];
     return os << "]";
 }
 
 template<typename T>
-void printMatrix(const string &message, vector<vector<T>> &matrice) {
-    cout << left << setw(15) << message << ": "
-         << matrice << endl;
+void printMatrix(const std::string &message, std::vector<std::vector<T>> &matrice) {
+    std::cout << std::left << std::setw(15) << message << ": "
+         << matrice << std::endl;
 
 }
 
 
 template<typename T>
-void shuffleVector(vector<T> &v) {
-
+void shuffleVector(std::vector<T> &v) {
     // Reference: https://cplusplus.com/reference/algorithm/shuffle/?kw=shuffle
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-
-    shuffle(v.begin(), v.end(), default_random_engine(seed));
+    shuffle(v.begin(), v.end(), std::default_random_engine(seed));
 
 }
 
 
 template<typename T>
-void shuffleRow(vector<T> &row) {
+void shuffleRow(std::vector<T> &row) {
     shuffleVector(row);
 }
 
 template<typename T>
-
-void shuffleMatrix(vector<vector<T>> &matrice) {
-
+void shuffleMatrix(std::vector<std::vector<T>> &matrice) {
     for_each(matrice.begin(), matrice.end(), shuffleRow<T>);;
-
     printMatrix(SHUFFLE_MRIX_TITLE, matrice);
-
 }
 
 template<typename T>
-bool compareMin(const vector<T>& v1, const vector<T>& v2){
+bool compareMin(const std::vector<T> &v1, const std::vector<T> &v2) {
     auto min1 = *min_element(v1.begin(), v1.end());
     auto min2 = *min_element(v2.begin(), v2.end());
     return min1 < min2;
 }
 
 template<typename T>
-void sortMatrix (vector<vector<T>>& matrice){ // par valeur ou par référence ??
+void sortMatrix(std::vector<std::vector<T>> &matrice) { // par valeur ou par référence ??
     sort(matrice.begin(), matrice.end(), compareMin<T>);
     printMatrix(SORT_MRIX_TITLE, matrice);
 }
 
 
+using namespace std;
 
 int main() {
 
